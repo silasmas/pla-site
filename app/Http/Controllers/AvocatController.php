0a6fcfeb7205e0e0abf5bd2e2b557a725840f600
silasmas <silasjmas@gmail.com>
2023-03-20 03:38:47 +0100

    jdjd

--- app/Http/Controllers/AvocatController.php
+++ app/Http/Controllers/AvocatController.php
@@ -41,12 +41,12 @@ class AvocatController extends Controller
 
         //   $pdf = public_path('storage/' . $req->cv);
 
-        $image = QrCode::format("png")->merge('https://plaafricalaw.com/public/assets/images/PLA_logo1.png', 1, true)
+        $image = QrCode::size(300)->format("png")->merge('https://plaafricalaw.com/public/assets/images/PLA_logo1.png', 1, true)
             ->generate("https://beraca.hardymuanda.com/qreunion.php?reunion=".$req);
            // $image->move('storage/qr/', $image);
 
          echo '<img src="data:image/png;base64,' . base64_encode($image) . '" alt="QR Code" />';
-        return base64_encode($image);
+
 
     }
     public function downloadCV(Request $req)
