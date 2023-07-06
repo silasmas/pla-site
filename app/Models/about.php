e948cb466e73539cff7d6e03391624f34a9b2b46
silasmas <silasjmas@gmail.com>
2023-07-06 01:41:49 +0100

    jj

--- app/Models/about.php
+++ app/Models/about.php
@@ -13,6 +13,6 @@ class about extends Model
     protected $guarded = [];
     protected $dates = ['created_at', 'updated_at'];
 
-    public $translatable = ['titre1', 'detail', 'titre2', 'temps',
+    public $translatable = ['titre1', 'detail', 'titre2', 'temps','titreBeneficesmall','resume','b1','r1','b2','r2','b3','r3','b4','r4','b5','r5',
         'h1', 'h2', 'detail2', 'detail3', 'quisommenous', 'titrecabinet', 'contenu', 'titreNosValeurs', 'slogant'];
 }
